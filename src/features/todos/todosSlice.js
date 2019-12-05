c7c2504686dac8288da1e2f221a7a4cbc67f243f
Shane Brinkman-Davis Delamore <shanebdavis@gmail.com>
2019-12-05 14:30:45 -0800

    ALL TESTS PASS

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -2,8 +2,9 @@ import { useRedux } from 'hooks-for-redux'
 
 let nextTodoId = 0
 
-export const [useTodos, { addTodo, toggleTodo }] = useRedux('todos', [], {
-  addTodo: (todos, text) => [...todos, { text, id: nextTodoId++ }],
+export const [useTodos, { addTodo, toggleTodo, resetTodos }, todos] = useRedux('todos', [], {
+  addTodo: (todos, text) => [...todos, { text, completed: false, id: nextTodoId++ }],
+  resetTodos: () => {nextTodoId = 0;return []},
   toggleTodo: (todos, id) =>
     todos.map(todo =>
       todo.id === id ? { ...todo, completed: !todo.completed } : todo
--- src/features/todos/todosSlice.spec.js
+++ src/features/todos/todosSlice.spec.js
@@ -1,139 +1,50 @@
-import todos, { addTodo, toggleTodo } from './todosSlice'
+import { addTodo, toggleTodo, resetTodos, todos } from './todosSlice'
 
 describe('todos reducer', () => {
   it('should handle initial state', () => {
-    expect(todos(undefined, {})).toEqual([])
+    expect(todos.getState()).toEqual([])
   })
 
   it('should handle ADD_TODO', () => {
-    expect(
-      todos([], {
-        type: addTodo.type,
-        payload: {
-          text: 'Run the tests',
-          id: 0
-        }
-      })
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 0
-      }
+    resetTodos()
+    addTodo("Run the tests")
+    expect(todos.getState()).toEqual([
+      { id: 0, text: 'Run the tests', completed: false }
+    ])
+    addTodo("Use Redux")
+    expect(todos.getState()).toEqual([
+      { id: 0, text: 'Run the tests', completed: false },
+      { id: 1, text: 'Use Redux', completed: false }
     ])
 
-    expect(
-      todos(
-        [
-          {
-            text: 'Run the tests',
-            completed: false,
-            id: 0
-          }
-        ],
-        {
-          type: addTodo.type,
-          payload: {
-            text: 'Use Redux',
-            id: 1
-          }
-        }
-      )
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 0
-      },
-      {
-        text: 'Use Redux',
-        completed: false,
-        id: 1
-      }
-    ])
-
-    expect(
-      todos(
-        [
-          {
-            text: 'Run the tests',
-            completed: false,
-            id: 0
-          },
-          {
-            text: 'Use Redux',
-            completed: false,
-            id: 1
-          }
-        ],
-        {
-          type: addTodo.type,
-          payload: {
-            text: 'Fix the tests',
-            id: 2
-          }
-        }
-      )
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: false,
-        id: 0
-      },
-      {
-        text: 'Use Redux',
-        completed: false,
-        id: 1
-      },
-      {
-        text: 'Fix the tests',
-        completed: false,
-        id: 2
-      }
+    addTodo("Fix the tests")
+    expect(todos.getState()).toEqual([
+      { id: 0, text: 'Run the tests', completed: false },
+      { id: 1, text: 'Use Redux', completed: false },
+      { id: 2, text: 'Fix the tests', completed: false }
     ])
   })
 
   it('should handle TOGGLE_TODO', () => {
-    expect(
-      todos(
-        [
-          {
-            text: 'Run the tests',
-            completed: false,
-            id: 1
-          },
-          {
-            text: 'Use Redux',
-            completed: false,
-            id: 0
-          }
-        ],
-        {
-          type: toggleTodo.type,
-          payload: 1
-        }
-      )
-    ).toEqual([
-      {
-        text: 'Run the tests',
-        completed: true,
-        id: 1
-      },
-      {
-        text: 'Use Redux',
-        completed: false,
-        id: 0
-      }
+    resetTodos()
+    addTodo("Run the tests")
+    addTodo("Use Redux")
+    toggleTodo(1)
+
+    expect(todos.getState()).toEqual([
+      { id: 0, text: 'Run the tests', completed: false },
+      { id: 1, text: 'Use Redux', completed: true }
     ])
   })
 })
 
 describe('addTodo', () => {
   it('should generate incrementing todo IDs', () => {
+    resetTodos()
     const action1 = addTodo('a')
     const action2 = addTodo('b')
 
-    expect(action1.payload).toEqual({ id: 0, text: 'a' })
-    expect(action2.payload).toEqual({ id: 1, text: 'b' })
+    expect(action1.payload).toEqual( 'a' )
+    expect(action2.payload).toEqual( 'b' )
   })
 })
